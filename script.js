385ab44babb567ba7f18425f272567566991339b
Pittab <120305267+Pittab@users.noreply.github.com>
2024-06-19 07:40:06 +0100

    Added a couple more themes, did the tiniest bit of refactoring of some JS and that's about it

--- script.js
+++ script.js
@@ -67,9 +67,8 @@ function initialise(){
 	if (getCookie("link1url")!="" && getCookie("link1content")!=""){
 		document.getElementById("link1").href = getCookie("link1url");
 		document.getElementById("link1").innerHTML = getCookie("link1content");
-	} else {
-		document.getElementById("favouriteHeading").style.display="none";
-	}
+		document.getElementById("favouriteHeading").style.display="block"
+	}	
 	if (getCookie("link2url")!="" && getCookie("link2content")!=""){
 		document.getElementById("link2").href = getCookie("link2url");
 		document.getElementById("link2").innerHTML = getCookie("link2content");
